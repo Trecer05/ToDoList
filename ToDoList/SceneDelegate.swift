8aa97585728f021e054a72443dd2b83f24661c28
Sergey Tretyakov <serzh.tretyakov.200505@mail.ru>
2026-07-31 00:01:45 +0500

    refactor: rename task list view controller

--- ToDoList/SceneDelegate.swift
+++ ToDoList/SceneDelegate.swift
@@ -13,7 +13,7 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             return
         }
 
-        let rootViewController = ViewController()
+        let rootViewController = TaskListViewController()
 
         let navigationController = UINavigationController(
             rootViewController: rootViewController
