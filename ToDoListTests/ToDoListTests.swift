25f9457e065f07395b054f04543059cf4aa579eb
Sergei <144253801+Trecer05@users.noreply.github.com>
2026-07-30 23:28:10 +0500

    Initial Commit

---
+++ ToDoListTests/ToDoListTests.swift
@@ -0,0 +1,19 @@
+//
+//  ToDoListTests.swift
+//  ToDoListTests
+//
+//  Created by Sergey Tretyakov on 30.07.2026.
+//
+
+import Testing
+@testable import ToDoList
+
+struct ToDoListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
